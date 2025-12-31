0399789cde7a9ee5584fc9b365119e47c76c57b8
jyothika1116 <pichikajyothika@gmail.com>
2025-12-31 15:27:07 +0530

    challenge1 workdone

---
+++ jyothika 6548/challenge1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>          
+int main()                  
+{
+    printf("Name: Alex\n");    
+    printf("Age: 15\n");        
+    printf("Country: Canada\n");
+
+    return 0;       
+
